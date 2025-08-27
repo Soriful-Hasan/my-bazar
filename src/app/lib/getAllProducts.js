6ef070dbace8f2a79f8b593025339780b4d49857
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-08-28 00:58:05 +0600

    base url fix

--- src/app/lib/getAllProducts.js
+++ src/app/lib/getAllProducts.js
@@ -1,12 +1,8 @@
-
 export async function getAllProducts() {
   try {
-    const res = await fetch(
-      `https://my-bazar-seven.vercel.app/api/allProductsData`,
-      {
-        cache: "no-store",
-      }
-    );
+    const res = await fetch("/api/allProductsData", {
+      cache: "no-store",
+    });
     return await res.json();
   } catch (error) {
     console.error("Error fetching products:", error);
--- src/app/lib/getProducts.js
+++ src/app/lib/getProducts.js
@@ -1,12 +1,9 @@
 // const baseUrl = process.env.NEXT_PUBLIC_NEXTAUTH_URL;
 export async function getProducts() {
   try {
-    const res = await fetch(
-      `https://my-bazar-seven.vercel.app/api/hotproducts`,
-      {
-        cache: "no-store",
-      }
-    );
+    const res = await fetch("/api/hotproducts", {
+      cache: "no-store",
+    });
     return await res.json();
   } catch (error) {
     console.error("Error fetching products:", error);
